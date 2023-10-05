0aa10926242279cb35142d0201159823d8c4824e
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-05 19:21:37 +0200

    add mixins and variables

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { motion } from "framer-motion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">hhhh</div>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
