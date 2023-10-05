37d1be8dde79cbe939a507b8738d6cd42e2f62f3
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-06 00:25:26 +0200

    add some images and create images object wrapper with the products demo

---
+++ src/productsData.js
@@ -0,0 +1,254 @@
+export const productsData = [
+  {
+    id: 2,
+    img: "./images/1.jpg",
+    name: "Cameras",
+    price: 600,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "other",
+  },
+  {
+    id: 1,
+    img: "./images/2.jpg",
+    name: "Minoxidils",
+    price: 40,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "other",
+  },
+  {
+    id: 3,
+    img: "./images/3.jpg",
+    name: "Blower",
+    price: 100,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "other",
+  },
+  {
+    id: 4,
+    img: "./images/audiBlack.jpg",
+    name: "Audii Black",
+    price: 60000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 5,
+    img: "./images/audiBlue.jpg",
+    name: "Audi Blue",
+    price: 100000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 6,
+    img: "./images/audiwhite.jpg",
+    name: "Audi White",
+    price: 30000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 7,
+    img: "./images/audiYellow.jpg",
+    name: "Audi Yellow",
+    price: 200000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 8,
+    img: "./images/beer.jpg",
+    name: "Mutzung",
+    price: 10,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "drinks",
+  },
+  {
+    id: 9,
+    img: "./images/benza.jpg",
+    name: "Brabus",
+    price: 120000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 10,
+    img: "./images/brabus.jpg",
+    name: "Brabus",
+    price: 600000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 11,
+    img: "./images/brabusBlue.jpg",
+    name: "Brabus Blue",
+    price: 600000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 12,
+    img: "./images/c.jpg",
+    name: "T-shirts",
+    price: 40,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "clothings",
+  },
+  {
+    id: 13,
+    img: "./images/c2.jpg",
+    name: "T-shirts MINO",
+    price: 200,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "clothings",
+  },
+  {
+    id: 14,
+    img: "./images/cheers.jpg",
+    name: "Beer KI",
+    price: 5,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "drinks",
+  },
+  {
+    id: 15,
+    img: "./images/cheersangele.jpg",
+    name: "Beer Plates",
+    price: 20,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "drinks",
+  },
+  {
+    id: 16,
+    img: "./images/eecfurni.jpg",
+    name: "Beds",
+    price: 1000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 17,
+    img: "./images/furfn1.jpg",
+    name: "Divine Chargers",
+    price: 2000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 18,
+    img: "./images/furn.jpg",
+    name: "Salon furnitures",
+    price: 800,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 19,
+    img: "./images/furni1.jpg",
+    name: "Office furnished",
+    price: 30.6,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 111,
+    img: "./images/goldfurni.jpg",
+    name: "Old Furnitures",
+    price: 3400,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 20,
+    img: "./images/insBrabus.jpg",
+    name: "Brabus Inside",
+    price: 800000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 21,
+    img: "./images/kingFurni.jpg",
+    name: "Kingdom Furnitures",
+    price: 1230,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 22,
+    img: "./images/mentionFurni.jpg",
+    name: "Wisdom furnished",
+    price: 5500,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 155,
+    img: "./images/mutzing.jpg",
+    name: "Family beer",
+    price: 15,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "drinks",
+  },
+  {
+    id: 1656,
+    img: "./images/redfurni.jpg",
+    name: "Love chair",
+    price: 100,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "furnitures",
+  },
+  {
+    id: 1323,
+    img: "./images/skiddin.jpg",
+    name: "Skidders",
+    price: 50000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 12323,
+    img: "./images/toy.jpg",
+    name: "Toyota Santafe",
+    price: 25000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+  {
+    id: 19797,
+    img: "./images/yoyota.jpg",
+    name: "Toyota Magni X",
+    price: 60000,
+    details:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quo aperiam",
+    category: "vehicles",
+  },
+];
