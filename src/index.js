192dd60eca639b83b92a1aa9e47d3ad304fe49b1
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-05 18:54:04 +0200

    create react-app and remove eb=verything to resember codepen

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
