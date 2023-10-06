e8a63a6aef70ec55da256385caf10afd5bf3ec73
nsanzimfura-eric <erickykress1@gmail.com>
2023-10-06 09:28:06 +0200

    add respo on media devices

--- src/index.js
+++ src/index.js
@@ -85,11 +85,11 @@ const Done = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_26_6)">
+    <g clipPath="url(#clip0_26_6)">
       <path
         d="M77.2959 30.7716L71.2314 22.36L43.9626 60.1825L50.0271 68.5941L77.2959 30.7716ZM95.5325 22.36L50.0271 85.4771L32.0486 60.6001L25.9841 69.0117L50.0271 102.36L101.64 30.7716L95.5325 22.36ZM1.64 69.0117L25.683 102.36L31.7475 93.9484L7.74753 60.6001L1.64 69.0117Z"
         fill="#0BFA31"
-      />
+      ></path>
     </g>
     <defs>
       <clipPath id="clip0_26_6">
@@ -111,7 +111,7 @@ const CartIcon = () => (
     <path
       d="M44 36H52V24H64V16H52V4H44V16H32V24H44V36ZM28 72C23.6 72 20.04 75.6 20.04 80C20.04 84.4 23.6 88 28 88C32.4 88 36 84.4 36 80C36 75.6 32.4 72 28 72ZM68 72C63.6 72 60.04 75.6 60.04 80C60.04 84.4 63.6 88 68 88C72.4 88 76 84.4 76 80C76 75.6 72.4 72 68 72ZM28.68 59L28.8 58.52L32.4 52H62.2C65.2 52 67.84 50.36 69.2 47.88L84.64 19.84L77.68 16H77.64L73.24 24L62.2 44H34.12L33.6 42.92L24.64 24L20.84 16L17.08 8H4V16H12L26.4 46.36L21 56.16C20.36 57.28 20 58.6 20 60C20 64.4 23.6 68 28 68H76V60H29.68C29.16 60 28.68 59.56 28.68 59Z"
       fill="#BC4823"
-    />
+    ></path>
   </svg>
 );
 
@@ -127,7 +127,7 @@ const DeleteIcon = () => (
     <path
       d="M4 64C4 68.4 7.6 72 12 72H44C48.4 72 52 68.4 52 64V16H4V64ZM56 4H42L38 0H18L14 4H0V12H56V4Z"
       fill="#FE0000"
-    />
+    ></path>
   </svg>
 );
 
@@ -386,10 +386,11 @@ function App() {
           <motion.h4
             initial={{ y: "-100vw" }}
             animate={{ y: 0 }}
-            transition={{ duration: 1, type: "spring", stiffness: 10 }}
+            transition={{ duration: 0.5, type: "spring", stiffness: 30 }}
             style={{ textAlign: "center" }}
           >
-            Welcome to Idea shop! Buy your favourite items at the lowest price
+            Welcome to <strong>Idea shop!</strong> Buy your favourite items at
+            the lowest price
           </motion.h4>
           <div
             className="allProducts"
@@ -398,7 +399,7 @@ function App() {
             <motion.h2
               initial={{ y: "-100vw" }}
               animate={{ y: 0 }}
-              transition={{ duration: 1.5, delay: 0.5, stiffness: 120 }}
+              transition={{ duration: 1.5, delay: 0.2, stiffness: 120 }}
             >
               Get you Favourites here
             </motion.h2>
